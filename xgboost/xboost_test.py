a0f9b2bc5086c6f08652e6591045a0569f2d633f
sleter <sleter97@gmail.com>
2018-11-09 16:25:35 +0100

    xgboost example

---
+++ xgboost/xboost_test.py
@@ -0,0 +1,27 @@
+from numpy import loadtxt
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+dataset = loadtxt('test.csv', delimiter=',')
+
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+#seed=?7
+test_size = 0.3
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size)#random_state=seed
+
+model = XGBClassifier()
+model.fit(X_train, y_train)
+
+print(model)
+
+y_pred = model.predict(X_test)
+predictions = [round(value) for value in y_pred]
+
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+
+
